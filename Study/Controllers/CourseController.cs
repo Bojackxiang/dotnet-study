78ea7b93a23bf6855b81424f54392d6f036351eb
Alex <alex@q-83.com>
2021-11-02 15:37:27 +1100

    错误验证和参数验证

--- Study/Controllers/CourseController.cs
+++ Study/Controllers/CourseController.cs
@@ -9,33 +9,6 @@ namespace Study.Controllers
     [ApiController]
     public class CourseController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            var newStringList = new List<string>();
-            newStringList.Add("hello");
-            return newStringList;
-        }
-
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        /**
-         * 【自动匹配】
-         * 一个经典的案列，
-         * 进来的参数会和我这边的 class CreateCourseParams 自动匹配
-         * 【Post Body】
-         * * {
-         * "CourseName": "math",
-         * "StudentsList": [
-         * {"StudentName": "Alex"},
-         * {"StudentName": "Eric"}
-         * ]
-         * }
-         */
         [HttpPost]
         public IActionResult Post([FromBody] CreateCourseParams value)
         {
@@ -44,10 +17,6 @@ namespace Study.Controllers
             foreach (var student in value.StudentsList) Console.WriteLine(student.StudentName);
             return Ok();
         }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
